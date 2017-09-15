bf192049e01a3ea00d320e710079e7d9d46c0de1
kubota-kohei <kubota-kohei@coworkplace.jp>
2017-09-15 13:07:39 +0900

    [create] template-header/footer detail-page

--- fiver/fiver/urls.py
+++ fiver/fiver/urls.py
@@ -19,5 +19,6 @@ from app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^detail/(?P<id>[0-9]+)/$', views.detail, name='detail'),
 ]
